ec0b2a672add7e45854d3aa6027fbf8ee963394e
BobbyCharms <robertcharming@Gmail.com>
2023-02-09 18:45:37 -0500

    attempted to generate password

--- Develop/script.js
+++ Develop/script.js
@@ -2,22 +2,29 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+
+// Identifies my variables to be used through the program 
 var lowercase = "abcdefghijklmnopqrstuvwxyz"
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var numeric = 1234567890
 var specialCharacters = "!@#$%^&*"
 var outcome  = ""
+var bank = ""
 
+// Function to generate the password 
 function generatePassword() {
+  // Confirming password length is within bounds 8-128 and rejecting input if not.
   var passwordLength = prompt("Please define the length of your password.");
   if (passwordLength < 8 || passwordLength > 128) {
     alert("Password length needs to be between 8 and 128 characters.")
     generatePassword()
+    // Confirming what variables should be present in the password generated. 
   } else {
     var lowercasePassword = confirm("Do you want lowercase letters in your password?")
     var uppercasePassword = confirm("Do you want uppercase letters in your password?")
     var numericPassword = confirm("Do you want numeric values in your password?")
     var specialPassword = confirm("Do you want special characters in your password?")
+    // Alerts user to select one of the variable types to move forward.
     if (!lowercasePassword && !uppercasePassword && !numericPassword && !specialPassword) {
       alert("Please select one type to confirm.")
       generatePassword()
@@ -34,8 +41,14 @@ function generatePassword() {
     if (specialPassword === true) {
       outcome += specialCharacters
     }
-  return outcome
+    // This should generate a password, but it's not operating properly. 
+    for(var i = 0, n = outcome.length; i < passwordLength.length; i++) {
+     outcome += outcome.charAt[Math.floor(Math.random() * n)];
+    }
+  } 
+  // Displays password in console and in the text box
   console.log(outcome)
+  return outcome
 }
 
 function writePassword() {
@@ -46,23 +59,6 @@ function writePassword() {
 }
 
 
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// Click generate password presents prompts for password criteria
-
-// I can select the criteria I want in the password
-
-// Selected criteria is validated 
-
-// The password is generated
-
-// The password is displayed on the page 
-
-// Criteria 
-// - length 8-128 characters
-// - confirm types of lowercase, uppercase, numeric, and/or special characters
-// - validate at least one character type must be selected
-// - when all prompts are answered, password is generated that matches
-// - password is displaced in alert or written to page 
